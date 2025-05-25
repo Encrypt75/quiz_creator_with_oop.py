538526f0ff14e349d32ec274e0d6741d10d1b629
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-05-25 16:34:25 +0800

    quiz_creator_with_oop_2
    bugs fixed

--- quiz_creator_with_oop.py
+++ quiz_creator_with_oop.py
@@ -1,13 +1,16 @@
 import json
 import os
+from colorama import init, Fore, Style
+
+init()
 
 #create class
 class QuizCreator:
-    def __init__(self):
-        pass
+    def __init__(self, file_load):
+        self.file_load = file_load
     
     #loads data from a json file
-    def file_load():
+    def file_load(self):
         if os.path.exists("json_text.json"):
             with open("json_text.json", "r") as file:
                 try:
@@ -17,3 +20,35 @@ class QuizCreator:
                 except json.JSONDecodeError:
                         pass
         return [] 
+    
+    def data_saver(self, data_format):
+        #open a json file to store the data
+        with open("json_text.json", "w") as file:
+            json.dump(data_format, file, indent=4)
+
+    def add_questions(self):
+        #callout file_load()
+        quiz_data = self.file_load()
+
+        #where the loop begins
+        while True: 
+            #ask user for inputs like question, options, and correct answer
+            questions = input(Fore.YELLOW + Style.BRIGHT + f"\nEnter question: " + Style.RESET_ALL)
+            options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
+            correct_answers = valid_input(f"Enter correct answer: ").lower()
+
+            #creates dictionary for users' input
+            data_format = {
+                "question": questions,
+                "option": options, 
+                "correct_answer": correct_answers
+            }
+
+            #then appends the input to the list
+            quiz_data.append(data_format)
+
+            try_again = input("add another question? (y/n): ").lower()
+            if try_again == "n" or try_again == "no":
+                break
+                
+        data_saver(quiz_data)
