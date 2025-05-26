052cd979e9017e559a3838a54b7df4ea67f4efc6
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-05-26 18:00:26 +0800

    quiz_creator_with_oop_final
    Back from the start, to make things organize and to fix all errors.

--- quiz_creator_with_oop.py
+++ quiz_creator_with_oop.py
@@ -1,54 +1,27 @@
 import json
 import os
-from colorama import init, Fore, Style
 
-init()
+#main class
+class QuizProgram:
+    def __init__(self):
+        self.filname = "json_text.json"
 
-#create class
-class QuizCreator:
-    def __init__(self, file_load):
-        self.file_load = file_load
-    
     #loads data from a json file
     def file_load(self):
-        if os.path.exists("json_text.json"):
-            with open("json_text.json", "r") as file:
+        if os.path.exists(self.filename):
+            with open(self.filename, "r") as file:
                 try:
                     quiz_data = json.load(file)
                     if isinstance(quiz_data, list):
                         return quiz_data
                 except json.JSONDecodeError:
-                        pass
-        return [] 
-    
+                    pass
+        return []
+
     def data_saver(self, data_format):
         #open a json file to store the data
-        with open("json_text.json", "w") as file:
+        with open(self.filename, "w") as file:
             json.dump(data_format, file, indent=4)
 
     def add_questions(self):
-        #callout file_load()
         quiz_data = self.file_load()
-
-        #where the loop begins
-        while True: 
-            #ask user for inputs like question, options, and correct answer
-            questions = input(Fore.YELLOW + Style.BRIGHT + f"\nEnter question: " + Style.RESET_ALL)
-            options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
-            correct_answers = valid_input(f"Enter correct answer: ").lower()
-
-            #creates dictionary for users' input
-            data_format = {
-                "question": questions,
-                "option": options, 
-                "correct_answer": correct_answers
-            }
-
-            #then appends the input to the list
-            quiz_data.append(data_format)
-
-            try_again = input("add another question? (y/n): ").lower()
-            if try_again == "n" or try_again == "no":
-                break
-                
-        data_saver(quiz_data)
